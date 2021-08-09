e59f3d6027921c5a16881ae416d5c25724807cf0
Subhashis <subhashisz@gmail.com>
2021-08-09 19:33:05 +0530

    message

--- src/main/java/com/myapp/ems/sql/queries/EmployeeQueryBuilder.java
+++ src/main/java/com/myapp/ems/sql/queries/EmployeeQueryBuilder.java
@@ -4,8 +4,15 @@ import com.myapp.ems.model.Employee;
 
 public class EmployeeQueryBuilder {
 
+	public static String EMS_QUERY_UDATE_EMPLOYEE_BY_ID = "update employees set email_id = :email_id where id = :id";
+	
 	public String updateEmployeeQuery(Employee emp) {
 		String QUERY_UPDATE = "";
+		if(emp!=null) {
+			if(emp.getFirstName().isEmpty()) {
+				
+			}
+		}
 
 		return QUERY_UPDATE;
 	}
